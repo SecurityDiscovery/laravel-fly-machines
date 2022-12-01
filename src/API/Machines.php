d2e2c2f069939d40bdba79f6d8485fe988bf3b5e
Sebastien Kaul <45340011+KaulSe@users.noreply.github.com>
2022-12-01 14:25:04 +0100

    add more machine endpoints

--- src/API/Machines.php
+++ src/API/Machines.php
@@ -2,86 +2,243 @@
 
 namespace SecurityDiscovery\LaravelFlyMachines\API;
 
-use Illuminate\Http\Client\PendingRequest;
-use Illuminate\Support\Facades\Http;
+use SecurityDiscovery\LaravelFlyMachines\Helpers\HTTPClient;
 
 class Machines
 {
-    public PendingRequest $client;
+    public HTTPClient $client;
+
+    private static string $FLY_NONCE_HEADER = 'fly-machine-lease-nonce';
 
     public function __construct(string $appName)
     {
-        $baseUrl = sprintf('%s://%s/v1/apps/%s',
-            config('fly-machines.proto'),
-            config('fly-machines.endpoint'),
-            $appName
-        );
-        $this->client = Http::withHeaders([
-            'Authorization' => 'Bearer '.config('fly-machines.token'),
-        ])
-            ->acceptJson()
-            ->baseUrl($baseUrl);
+        $this->client = new HTTPClient($appName);
     }
 
     /**
      * List Machines of a Fly.io App.
      *
-     * @throws \Illuminate\Http\Client\RequestException
-     */
-    public function list()
-    {
-        return $this->client->get('/machines')->throw()->json();
-    }
-
-    /**
-     * Launch a Fly machine.
+     * @return mixed
      *
      * @throws \Illuminate\Http\Client\RequestException
      */
-    public function launch(array $machine)
+    public function list(): mixed
     {
-        return $this->client->post('/machines', $machine)->throw()->json();
+        return $this->client->get(url: '/machines');
     }
 
     /**
-     * Get a Fly machine using the machine id.
+     * Launch a machine.
+     *
+     * @param  array  $machine The machine config.
+     * @return mixed
      *
      * @throws \Illuminate\Http\Client\RequestException
      */
-    public function get(string $machineId)
+    public function launch(array $machine): mixed
     {
-        return $this->client->get('/machines/'.$machineId)->throw()->json();
+        return $this->client->post(url: '/machines', data: $machine);
     }
 
     /**
-     * Stop a Fly machine using the machine id.
+     * Update a machine.
+     *
+     * @param  string  $machineId The machine id.
+     * @param  array  $machine The machine config.
+     * @param  string  $nonce The nonce. If == '', we don't send it.
+     * @return mixed
      *
      * @throws \Illuminate\Http\Client\RequestException
      */
-    public function stop(string $machineId)
+    public function update(string $machineId, array $machine, string $nonce = ''): mixed
     {
-        return $this->client->post('/machines/'.$machineId.'/stop')->throw()->json();
+        $headers = [];
+        if ($nonce != '') {
+            $headers = [self::$FLY_NONCE_HEADER => $nonce];
+        }
+
+        return $this->client
+                ->withHeaders($headers)
+                ->post(url: '/machines/'.$machineId, data: $machine);
     }
 
     /**
-     * Start a Fly machine using the machine id.
+     * Get a machine.
+     *
+     * @param  string  $machineId The machine id.
+     * @return mixed
      *
      * @throws \Illuminate\Http\Client\RequestException
      */
-    public function start(string $machineId)
+    public function get(string $machineId): mixed
     {
-        return $this->client->post('/machines/'.$machineId.'/start')->throw()->json();
+        return $this->client->get(url: '/machines/'.$machineId);
     }
 
     /**
-     * Delete a Fly machine by their machine id.
+     * Stop a machine.
+     *
+     * @param  string  $machineId The machine id.
+     * @return mixed
      *
      * @throws \Illuminate\Http\Client\RequestException
      */
-    public function destroy(string $machineId, bool $kill)
+    public function stop(string $machineId): mixed
     {
-        $urlParameters = http_build_query(['kill' => $kill]);
+        return $this->client->post(url: '/machines/'.$machineId.'/stop');
+    }
 
-        return $this->client->delete('/machines/'.$machineId.'?'.$urlParameters)->throw()->json();
+    /**
+     * Start a machine.
+     *
+     * @param  string  $machineId The machine id.
+     * @return mixed
+     *
+     * @throws \Illuminate\Http\Client\RequestException
+     */
+    public function start(string $machineId): mixed
+    {
+        return $this->client->post(url: '/machines/'.$machineId.'/start');
+    }
+
+    /**
+     * Send a signal to a machine.
+     *
+     * @param  string  $machineId The machine id.
+     * @param  int  $signal The signal to send. E.g. SIGKILL = 9.
+     * @return mixed
+     *
+     * @throws \Illuminate\Http\Client\RequestException
+     */
+    public function signal(string $machineId, int $signal): mixed
+    {
+        return $this->client->post(url: '/machines/'.$machineId.'/signal', data: [
+            'signal' => $signal,
+        ]);
+    }
+
+    /**
+     * Kill a machine using the signal 9 (SIGKILL).
+     *
+     * @param  string  $machineId The machine id.
+     * @return mixed
+     *
+     * @throws \Illuminate\Http\Client\RequestException
+     */
+    public function kill(string $machineId): mixed
+    {
+        return $this->signal(machineId: $machineId, signal: 9);
+    }
+
+    /**
+     * Restart a machine.
+     *
+     * @param  string  $machineId The machine id.
+     * @param  bool  $forceStop Force stop the machine.
+     * @param  int  $timeout Timeout. If == -1, we don't send that value.
+     * @param  string  $signal The signal. If == '', we don't send that value.
+     * @return mixed
+     *
+     * @throws \Illuminate\Http\Client\RequestException
+     */
+    public function restart(string $machineId, bool $forceStop, int $timeout = -1, string $signal = ''): mixed
+    {
+        $queryParams = [
+            'force_stop' => $forceStop,
+        ];
+        if ($timeout != -1) {
+            $queryParams['timeout'] = $timeout;
+        }
+        if ($signal != '') {
+            $queryParams['signal'] = $signal;
+        }
+
+        return $this->client->post(url: '/machines/'.$machineId.'/restart?'.http_build_query($queryParams));
+    }
+
+    /**
+     * Find a lease for a machine.
+     *
+     * @param  string  $machineId The machine id.
+     * @return mixed
+     *
+     * @throws \Illuminate\Http\Client\RequestException
+     */
+    public function findLease(string $machineId): mixed
+    {
+        return $this->client->get(url: '/machines/'.$machineId.'/lease');
+    }
+
+    /**
+     * Lease a machine.
+     *
+     * @param  string  $machineId The machine id.
+     * @param  int  $ttl If ttl != -1, we send the value.
+     * @return mixed
+     *
+     * @throws \Illuminate\Http\Client\RequestException
+     */
+    public function acquireLease(string $machineId, int $ttl = -1): mixed
+    {
+        $queryParams = [];
+        if ($ttl != -1) {
+            $queryParams['ttl'] = $ttl;
+        }
+
+        return $this->client->post(url: '/machines/'.$machineId.'/lease?'.http_build_query($queryParams));
+    }
+
+    /**
+     * Lease a machine.
+     *
+     * @param  string  $machineId The machine id.
+     * @param  string  $nonce The nonce. If == '', we don't send it.
+     * @return mixed
+     *
+     * @throws \Illuminate\Http\Client\RequestException
+     */
+    public function releaseLease(string $machineId, string $nonce = ''): mixed
+    {
+        $headers = [];
+        if ($nonce != '') {
+            $headers = [self::$FLY_NONCE_HEADER => $nonce];
+        }
+
+        return $this->client->withHeaders(headers: $headers)
+            ->delete(url: '/machines/'.$machineId.'/lease');
+    }
+
+    /**
+     * Wait for a machine.
+     *
+     * @param  string  $machineId The machine id.
+     * @param  string  $instanceId The machine instance id.
+     * @param  string  $state The machine state to wait for. Default is "started".
+     * @param  int  $timeout How long to wait for the state.
+     * @return mixed
+     *
+     * @throws \Illuminate\Http\Client\RequestException
+     */
+    public function wait(string $machineId, string $instanceId, string $state = "started", int $timeout = 30): mixed
+    {
+        $queryParams = [
+            'instance_id' => $instanceId,
+            'timeout' => $timeout,
+            'state' => $state,
+        ];
+
+        return $this->client->get(url: '/machines/'.$machineId.'/wait', query: $queryParams);
+    }
+
+    /**
+     * Delete a machine by their machine id.
+     *
+     * @throws \Illuminate\Http\Client\RequestException
+     */
+    public function destroy(string $machineId, bool $kill): mixed
+    {
+        $queryParams = http_build_query(['kill' => $kill]);
+
+        return $this->client->delete(url: '/machines/'.$machineId.'?'.$queryParams);
     }
 }
---
+++ src/Helpers/HTTPClient.php
@@ -0,0 +1,135 @@
+<?php
+
+namespace SecurityDiscovery\LaravelFlyMachines\Helpers;
+
+use Illuminate\Http\Client\PendingRequest;
+
+class HTTPClient extends PendingRequest
+{
+    public function __construct(string $appName)
+    {
+        parent::__construct();
+
+        $baseUrl = sprintf('%s://%s/v1/apps/%s',
+            config('fly-machines.proto'),
+            config('fly-machines.endpoint'),
+            $appName
+        );
+
+        $this->baseUrl($baseUrl);
+        $this->withHeaders([
+            'Authorization' => 'Bearer '.config('fly-machines.token'),
+        ]);
+        $this->acceptJson();
+    }
+
+    /**
+     * Issue a GET request to the given URL.
+     *
+     * @param  string  $url
+     * @param  array|string|null  $query
+     * @return mixed
+     *
+     * @throws \Illuminate\Http\Client\RequestException
+     */
+    public function get(string $url, $query = null): mixed
+    {
+        return $this->send('GET', $url, func_num_args() === 1 ? [] : [
+            'query' => $query,
+        ]);
+    }
+
+    /**
+     * Issue a HEAD request to the given URL.
+     *
+     * @param  string  $url
+     * @param  array|string|null  $query
+     * @return mixed
+     *
+     * @throws \Illuminate\Http\Client\RequestException
+     */
+    public function head(string $url, $query = null): mixed
+    {
+        return $this->send('HEAD', $url, func_num_args() === 1 ? [] : [
+            'query' => $query,
+        ]);
+    }
+
+    /**
+     * Issue a POST request to the given URL.
+     *
+     * @param  string  $url
+     * @param  array  $data
+     * @return mixed
+     *
+     * @throws \Illuminate\Http\Client\RequestException
+     */
+    public function post(string $url, $data = []): mixed
+    {
+        return $this->send('POST', $url, [
+            $this->bodyFormat => $data,
+        ]);
+    }
+
+    /**
+     * Issue a PATCH request to the given URL.
+     *
+     * @param  string  $url
+     * @param  array  $data
+     * @return mixed
+     *
+     * @throws \Illuminate\Http\Client\RequestException
+     */
+    public function patch($url, $data = []): mixed
+    {
+        return $this->send('PATCH', $url, [
+            $this->bodyFormat => $data,
+        ]);
+    }
+
+    /**
+     * Issue a PUT request to the given URL.
+     *
+     * @param  string  $url
+     * @param  array  $data
+     * @return mixed
+     *
+     * @throws \Illuminate\Http\Client\RequestException
+     */
+    public function put($url, $data = []): mixed
+    {
+        return $this->send('PUT', $url, [
+            $this->bodyFormat => $data,
+        ]);
+    }
+
+    /**
+     * Issue a DELETE request to the given URL.
+     *
+     * @param  string  $url
+     * @param  array  $data
+     * @return mixed
+     *
+     * @throws \Illuminate\Http\Client\RequestException
+     */
+    public function delete($url, $data = []): mixed
+    {
+        return $this->send('DELETE', $url, empty($data) ? [] : [
+            $this->bodyFormat => $data,
+        ]);
+    }
+
+    /**
+     * @param  string  $method
+     * @param  string  $url
+     * @param  array  $options
+     * @return mixed
+     *
+     * @throws \Illuminate\Http\Client\RequestException
+     * @throws \Exception
+     */
+    public function send(string $method, string $url, array $options = []): mixed
+    {
+        return parent::send($method, $url, $options)->throw()->json();
+    }
+}
