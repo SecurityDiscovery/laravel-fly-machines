e125fb96b44509243ac5feb15450a1bb4d94ff31
Sebastien Kaul <45340011+KaulSe@users.noreply.github.com>
2023-03-09 10:55:04 +0100

    use api.machines.dev as the api endpoint
    https://community.fly.io/t/flyctl-now-uses-api-machines-dev-by-default/11317

--- config/fly-machines.php
+++ config/fly-machines.php
@@ -2,9 +2,9 @@
 
 // config for SecurityDiscovery/LaravelFlyMachines
 return [
-    'proto' => env('FLY_API_PROTO', 'http'),
+    'proto' => env('FLY_API_PROTO', 'https'),
     // The endpoint to the Fly machines API.
-    'endpoint' => env('FLY_API_HOSTNAME', '127.0.0.1:4280'),
+    'endpoint' => env('FLY_API_HOSTNAME', 'api.machines.dev'),
     // The token to authenticate to the API.
     'token' => env('FLY_API_TOKEN'),
 ];
